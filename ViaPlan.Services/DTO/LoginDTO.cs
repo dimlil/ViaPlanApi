00dec9f6f7b24b459a607290541554681f23e969
DimVasDev <d.v.vassilev@gmail.com>
2025-05-11 02:24:39 +0300

    feat/ removed username from login

--- ViaPlan.Services/DTO/LoginDTO.cs
+++ ViaPlan.Services/DTO/LoginDTO.cs
@@ -3,7 +3,6 @@ namespace ViaPlan.Services.DTO
     public class LoginDTO
     {
         public string Email { get; set; } = string.Empty;
-        public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
     }
 }
