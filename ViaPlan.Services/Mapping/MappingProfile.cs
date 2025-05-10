21e2165c48849b8d26fff6104aea5b3a173bc0cc
DimVasDev <d.v.vassilev@gmail.com>
2025-05-10 14:34:07 +0300

    feat/ added auto mapper

---
+++ ViaPlan.Services/Mapping/MappingProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using ViaPlan.Entities;
+using ViaPlan.Services.DTO;
+
+namespace ViaPlan.Services.Mapping
+{
+    public class MappingProfile : Profile
+    {
+       public MappingProfile()
+        {
+            CreateMap<Trip, TripDTO>().ReverseMap();
+            CreateMap<User, UserDTO>();
+        }
+    }
+}
